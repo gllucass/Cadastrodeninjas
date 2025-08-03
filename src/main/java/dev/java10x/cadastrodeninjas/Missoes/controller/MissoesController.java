6e70deb8c9b91098f3a29c7e303dfc43a48db31b
gllucass <geraldolucas10@gmail.com>
2025-08-02 23:17:00 -0300

    feat: Criado rota para listar todas as missões

--- src/main/java/dev/java10x/cadastrodeninjas/Missoes/controller/MissoesController.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Missoes/controller/MissoesController.java
@@ -1,31 +1,43 @@
 package dev.java10x.cadastrodeninjas.Missoes.controller;
 
+import dev.java10x.cadastrodeninjas.Missoes.model.MissoesModel;
+import dev.java10x.cadastrodeninjas.Missoes.service.MissoesService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("missoes/")
+@RequestMapping("/missoes")
 public class MissoesController {
 
+
+    private MissoesService missoesService;
+
+    public MissoesController(MissoesService missoesService) {
+        this.missoesService = missoesService;
+    }
+
     //GET -- Mandar uma requisicao para mostrar as missoes
-    @GetMapping("listar")
-    public String mostrarMissao(){
-        return "Missao Listada com sucesso";
+    @GetMapping("/listar")
+    public List<MissoesModel> listarMissoes(){
+        return missoesService.listarMissoes();
     }
 
     //POST -- Mandar uma requisicao para criar as missoes
-    @PostMapping("criar")
+    @PostMapping("/criar")
     public String criarMissao(){
         return "Missao Criada com sucesso";
     }
 
     //PUT -- Mandar uma requisicao para alterar as missoes
-    @PutMapping("alterar")
+    @PutMapping("/alterar")
     public String alterarMissao(){
         return "Missao Alterada com sucesso";
     }
 
     //DELETE -- Mandar uma requisicao para deletar as missoes
-    @DeleteMapping("deletar")
+    @DeleteMapping("/deletar")
     public String deletarMissao(){
         return "Missao Deletada com sucesso";
     }
--- src/main/java/dev/java10x/cadastrodeninjas/Missoes/service/MissoesService.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Missoes/service/MissoesService.java
@@ -1,4 +1,23 @@
 package dev.java10x.cadastrodeninjas.Missoes.service;
 
+import dev.java10x.cadastrodeninjas.Missoes.model.MissoesModel;
+import dev.java10x.cadastrodeninjas.Missoes.repository.MissoesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MissoesService {
+
+    private MissoesRepository missoesRepository;
+
+    public MissoesService(MissoesRepository missoesRepository) {
+        this.missoesRepository = missoesRepository;
+    }
+
+    //Listar todas as missoes
+    public List<MissoesModel> listarMissoes(){
+        return missoesRepository.findAll();
+    }
 }
